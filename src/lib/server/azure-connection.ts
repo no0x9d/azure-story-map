93a5df117c8e5fd165457b16a425db963e3193c6
Christoph Hoffmann <christoph.hoffmann@adesso.de>
2026-03-23 15:33:08 +0100

    feat: allow to only change token or url

--- src/lib/server/azure-connection.ts
+++ src/lib/server/azure-connection.ts
@@ -4,34 +4,38 @@ import { secrets } from 'bun';
 const SERVICE_NAME = 'noOx9d/azure-story-map';
 
 export async function createConnection(orgUrl: string, token: string) {
-	const authHandler = azure.getPersonalAccessTokenHandler(token);
-	return new azure.WebApi(orgUrl, authHandler);
+  const authHandler = azure.getPersonalAccessTokenHandler(token);
+  return new azure.WebApi(orgUrl, authHandler);
 }
 
 export async function persistCredentials(orgUrl: string, token: string) {
-	await secrets.set({
-		service: SERVICE_NAME,
-		name: 'azure-base-url',
-		value: orgUrl
-	});
-	await secrets.set({
-		service: SERVICE_NAME,
-		name: 'azure-token',
-		value: token
-	});
+  if (orgUrl) {
+    await secrets.set({
+      service: SERVICE_NAME,
+      name: 'azure-base-url',
+      value: orgUrl
+    });
+  }
+  if (token) {
+    await secrets.set({
+      service: SERVICE_NAME,
+      name: 'azure-token',
+      value: token
+    });
+  }
 }
 
 export async function getCredentials() {
-	const orgUrl = await secrets.get({
-		service: SERVICE_NAME,
-		name: 'azure-base-url'
-	});
-	const token = await secrets.get({
-		service: SERVICE_NAME,
-		name: 'azure-token'
-	});
-	if (!orgUrl || !token) {
-		return null;
-	}
-	return { orgUrl, token };
+  const orgUrl = await secrets.get({
+    service: SERVICE_NAME,
+    name: 'azure-base-url'
+  });
+  const token = await secrets.get({
+    service: SERVICE_NAME,
+    name: 'azure-token'
+  });
+  if (!orgUrl || !token) {
+    return null;
+  }
+  return { orgUrl, token };
 }
